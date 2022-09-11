7d79052780e747ea7a287664a7f805a02db31821
Sanyam-Maheshwari <101491159+Sanyam-Maheshwari@users.noreply.github.com>
2022-09-11 23:42:28 +0530

    Time: 9 ms (8.43%), Space: 42 MB (18.14%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Solution {
+    // Scanner sc = new Scanner(System.in);
+    // int x = sc.nexttInt();
+    public int reverse(int x)
+    {
+        
+        if(x==0)
+            return 0;
+        long reverse=0;
+        while(x!=0)
+        {
+            int last_digit = x%10;
+            reverse = reverse*10+last_digit;
+            x=x/10;
+        }
+        System.out.println(reverse);
+        
+        
+        if(reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) {
+        return 0;
+        }
+        else
+            // System.out.println((int)reverse);
+            return (int)reverse;
+
+       
+    }
+}
