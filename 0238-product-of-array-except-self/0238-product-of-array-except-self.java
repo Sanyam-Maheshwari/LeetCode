aa9d4918b55bedab9eb62ebe40dc28ce58fef82f
Sanyam-Maheshwari <101491159+Sanyam-Maheshwari@users.noreply.github.com>
2022-11-17 23:37:44 +0530

    Time: 10 ms (28.62%), Space: 57.8 MB (52.52%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,27 +1,54 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        
         int n=nums.length;
-        int []prefix =new int[n];
-        prefix[0]=nums[0];
-        for(int i=1;i<n;i++)
+        int []ans=new int[n];
+        
+        int product=1;
+        for(int i=0;i<n;i++)
         {
-            prefix[i]=prefix[i-1]*nums[i];
+            product=product*nums[i];
+            ans[i]=product;
         }
         
-        int []suffix = new int[n];
-        suffix[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--)
+        product=1;
+        for(int i=n-1;i>0;i--)
         {
-            suffix[i]=suffix[i+1]*nums[i];
+            ans[i]=ans[i-1]*product;
+            product*=nums[i];
+            
         }
-        
-        int []ans =new int[n];
-        ans[0]=suffix[1];
-        ans[n-1]=prefix[n-2];
-        for(int i=1;i<n-1;i++)
-        {
-            ans[i]=prefix[i-1]*suffix[i+1];
-        }
+        ans[0]=product;
+        // int product=1;
+        // for(int i=n-1;i>0;i--)
+        // {
+        //     ans[i]=ans[i-1]*product;
+        //     product=nums[i]*product;
+        // }
         return ans;
+        
+//         int n=nums.length;
+//         int []prefix =new int[n];
+//         prefix[0]=nums[0];
+//         for(int i=1;i<n;i++)
+//         {
+//             prefix[i]=prefix[i-1]*nums[i];
+//         }
+        
+//         int []suffix = new int[n];
+//         suffix[n-1]=nums[n-1];
+//         for(int i=n-2;i>=0;i--)
+//         {
+//             suffix[i]=suffix[i+1]*nums[i];
+//         }
+        
+//         int []ans =new int[n];
+//         ans[0]=suffix[1];
+//         ans[n-1]=prefix[n-2];
+//         for(int i=1;i<n-1;i++)
+//         {
+//             ans[i]=prefix[i-1]*suffix[i+1];
+//         }
+//         return ans;
     }
 }
