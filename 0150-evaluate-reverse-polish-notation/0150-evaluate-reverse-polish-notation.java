41cd2e97505edfe5b75b019c511b762032f9cfee
Sanyam-Maheshwari <101491159+Sanyam-Maheshwari@users.noreply.github.com>
2022-11-20 14:19:59 +0530

    Time: 5 ms (97.60%), Space: 42.2 MB (84.62%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,68 +1,35 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stk=new Stack();
-        // for(int i=0;i<tokens.length;i++)
-        for(String s:tokens)
-        {       
-                if(s.equals("+"))
-                {
-                    int b=stk.pop();
-                    int a=stk.pop();
-                    stk.push(a+b);
-                }
-                else if(s.equals("-"))
-                {
-                    int b=stk.pop();
-                    int a=stk.pop();
-                    stk.push((a-b));
-                }
-                else if(s.equals("*"))
-                {
-                    int b=stk.pop();
-                    int a=stk.pop();
-                    stk.push(a*b);
-                }
-                else if(s.equals("/"))
-                {
-                    int b=stk.pop();
-                    int a=stk.pop();
-                    stk.push(a/b);
-                }
-            else
+    Stack<Integer> stk=new Stack();
+    for(String s:tokens)
+    {       
+            if(s.equals("+"))
             {
-                stk.push(Integer.parseInt(s));
+                int b=stk.pop();
+                int a=stk.pop();
+                stk.push(a+b);
             }
-                
+            else if(s.equals("-"))
+            {
+                int b=stk.pop();
+                int a=stk.pop();
+                stk.push((a-b));
             }
-            
-//             if(tokens[i]!="+" || tokens[i]!="-" || tokens[i]!="*" || tokens[i]!="/")
-//             {
-//                 stk.push(Integer.parseInt(tokens[i]));
-//             }
-           
-//             else
-//             { 
-//                 String sign=tokens[i];
-//                 int b=stk.pop();
-//                 int a=stk.pop();
-//                 if(sign=="+")
-//                 {
-//                     stk.push(a+b);
-//                 }
-//                 if(sign=="-")
-//                 {
-//                     stk.push(a-b);
-//                 }
-//                 if(sign=="*")
-//                 {
-//                     stk.push(a*b);
-//                 }
-//                 if(sign=="/")
-//                 {
-//                     stk.push(a/b);
-//                 }
-                
-//             }
+            else if(s.equals("*"))
+            {
+                int b=stk.pop();
+                int a=stk.pop();
+                stk.push(a*b);
+            }
+            else if(s.equals("/"))
+            {
+                int b=stk.pop();
+                int a=stk.pop();
+                stk.push(a/b);
+            }
+            else
+                stk.push(Integer.parseInt(s));   
+        }
         return stk.pop();
     }
 }
