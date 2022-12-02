dac3cc18ff94ee10d1e51672b07649d566c7b9aa
Sanyam-Maheshwari <101491159+Sanyam-Maheshwari@users.noreply.github.com>
2022-12-02 23:30:13 +0530

    Time: 12 ms (27.13%), Space: 47 MB (30.44%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
@@ -1,25 +1,50 @@
 class Solution {
     public List<Integer> intersection(int[][] nums) {
         HashMap<Integer, Integer> map=new HashMap();
+        List<Integer> list=new ArrayList();
         for(int i=0;i<nums.length;i++)
         {
             for(int j=0;j<nums[i].length;j++)
             {
                 map.put(nums[i][j],map.getOrDefault(nums[i][j],0)+1);
+                if(map.getOrDefault(nums[i][j],0)==nums.length)
+                {
+                    list.add(nums[i][j]);
+                }
             }
         }
-        List<Integer> list=new ArrayList();
-        for(Map.Entry<Integer,Integer> entry:map.entrySet())
-        {
-            if(entry.getValue()==nums.length)
-                list.add(entry.getKey());
-        }
+
+        // for(Map.Entry<Integer,Integer> entry:map.entrySet())
+        // {
+        //     if(entry.getValue()==nums.length)
+        //         list.add(entry.getKey());
+        // }
         Collections.sort(list);
         return list;
-        // int []ans=new int[list.size()];
-        // int i=0;
-        // for(Integer j:list)
-        //     ans[i++]=j;
-        // return ans;
     }
 }
+// class Solution {
+//     public List<Integer> intersection(int[][] nums) {
+//         HashMap<Integer, Integer> map=new HashMap();
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             for(int j=0;j<nums[i].length;j++)
+//             {
+//                 map.put(nums[i][j],map.getOrDefault(nums[i][j],0)+1);
+//             }
+//         }
+//         List<Integer> list=new ArrayList();
+//         for(Map.Entry<Integer,Integer> entry:map.entrySet())
+//         {
+//             if(entry.getValue()==nums.length)
+//                 list.add(entry.getKey());
+//         }
+//         Collections.sort(list);
+//         return list;
+//         // int []ans=new int[list.size()];
+//         // int i=0;
+//         // for(Integer j:list)
+//         //     ans[i++]=j;
+//         // return ans;
+//     }
+// }
