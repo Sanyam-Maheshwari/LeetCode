b93637c9b0189ac4f8455411c14f094dfef73d30
Sanyam-Maheshwari <101491159+Sanyam-Maheshwari@users.noreply.github.com>
2022-12-03 23:55:36 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (88.07%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        int []freq=new int[26];
+        for(char c:s.toCharArray())
+        {
+            if(freq[c-'a']==1)
+                return c;
+            freq[c-'a']++;
+        }
+        return '#';
+    }
+}
